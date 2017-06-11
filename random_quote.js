7fbb37e591ea2607d9904f8b34d52e20ae11312c
Szwarc Adam <adam.szwarc@nokia.com>
2017-06-11 11:23:10 +0200

    Added place for quote author. Updating html to show fetched quote.

--- random_quote.js
+++ random_quote.js
@@ -1,10 +1,15 @@
-
-
-
 $(document).ready(function() {
   $(clickMeButton).click(function() {
-    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(response) {
-      console.log(JSON.stringify(response));
-    });
+    $.ajax({
+      url : "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+      dataType : "jsonp",
+      success : function(json) {
+        $("#quote").html(json.quoteText);
+        $("#quoteAuthor").html(json.quoteAuthor);
+      },
+      error : function(xhr, status) {
+        alert("Request failed!");
+      }
     });
   });
+});
